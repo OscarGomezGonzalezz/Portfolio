24bcb167d7bb41543668f0241daa82dabf0e4fdd
OscarGomezGonzalezz <oscgomgon.business@gmail.com>
2025-08-19 21:03:45 +0200

    fixes for phone display

--- plugins/aosLibrary.client.js
+++ plugins/aosLibrary.client.js
@@ -1,12 +1,22 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-export default defineNuxtPlugin(nuxtApp => {
-    nuxtApp.AOS = new AOS.init({
-        //disable: window.innerWidth < 640,
-        offset: -100,
-        duration: 800,
-        easing: 'ease-in-out-cubic',
-        once: true
+export default defineNuxtPlugin((nuxtApp) => {
+  const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
+  nuxtApp.AOS = new AOS.init({
+    offset: -100,
+    duration: 800,
+    easing: 'ease-in-out-cubic',
+    once: true,
+    // Forzar animación en móviles aunque no haya scroll
+    startEvent: isMobile ? 'load' : 'DOMContentLoaded'
+  });
+
+  // Para móviles, forzar refresh después de load
+  if (isMobile) {
+    window.addEventListener('load', () => {
+      AOS.refresh();
     });
+  }
 });
